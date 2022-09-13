c5704d5b8d61af74eb87a7799f9c020d682b6144
beforeold <ibeforeold@gmail.com>
2022-09-13 16:41:09 +0800

    add Principle

---
+++ LearnDesignPattern/DesignPattern/Principle.swift
@@ -0,0 +1,31 @@
+//
+//  Principle.swift
+//  DesignPattern
+//
+//  Created by beforeold on 2022/9/13.
+//
+
+import Foundation
+
+class Principle {
+  // SOLID
+  // SRP：单一职责原则
+  
+  // OCP：开闭原则：对修改关闭，对扩展开放
+  
+  // LSP：里氏替换
+  
+  // ISP：接口隔离原则
+  
+  // DIP：依赖反转原则
+  
+  
+  // ===
+  // KISS：保持简单
+  
+  // YAGNI：可能用不上
+  
+  // DRY：不要重复自己的代码
+  
+  // LOD：高内聚，低耦合 Least Knowledge Principle
+}
