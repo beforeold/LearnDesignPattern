33a472f97fae85ee4443059b8fdb884aa839256c
beforeold <ibeforeold@gmail.com>
2022-09-13 17:53:41 +0800

    add Singleton

---
+++ LearnDesignPattern/DesignPattern/Singleton.swift
@@ -0,0 +1,16 @@
+//
+//  Singleton.swift
+//  DesignPattern
+//
+//  Created by beforeold on 2022/9/13.
+//
+
+import Foundation
+
+class Singleton {
+  static let shared = Singleton()
+  
+  private init() {
+    
+  }
+}
